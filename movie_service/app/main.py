f63d880d24a73de7d59f0d24f3b3f49147487406
bocon <boconlonton111@gmail.com>
2022-05-14 13:10:54 +0700

    Update - update docs url for each service

--- movie_service/app/main.py
+++ movie_service/app/main.py
@@ -4,11 +4,14 @@ from app.api.movies import movies
 
 from app.api.db import metadata, database, engine
 
-app = FastAPI()
+app = FastAPI(openapi_url="/api/v1/movies/openapi.json",
+              docs_url="/api/v1/movies/docs")
+
 
 @app.on_event("startup")
 async def startup():
     await database.connect()
+
 
 @app.on_event("shutdown")
 async def shutdown():
